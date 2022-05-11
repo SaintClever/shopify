b80a5f8198ccef3009b1c085c6f9a86d2b967f2e
Saint. Clever <parchment@gmx.com>
2022-05-11 10:31:58 -0400

    update app

--- inventory_app_shopify.py
+++ inventory_app_shopify.py
@@ -1,10 +1,15 @@
+from venv import create
+
+
 class App():
     def __init__(self, inventory = {}):
         self.inventory = inventory
     
+
     def create_item(self):
         item = input('Add item: ')
         self.inventory[item] = 1
+
 
     def edit_item(self):
         item, item_amount = input('add or subtract item (Ex: item name: amount): ').split()
@@ -15,8 +20,27 @@ class App():
         item = input('Delete item (are you sure): ')
         self.inventory(item, None)
 
+
     def view_iventory(self):
         print(self.inventory)
 
 
+    def start_app(self):
+        method = input('Inventory options: create | edit | delete | view | quit')
+
+        if method == 'create':
+            self.create_item()
+        elif method == 'edit':
+            self.edit_item()
+        elif method == 'delete':
+            self.delete_item()
+        elif method == 'view':
+            self.view_iventory()
+        elif method == 'quit':
+            quit()
+
+        self.start_app()
+
+
 app = App()
+app.start_app()
